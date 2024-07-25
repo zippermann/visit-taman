409c0affcfe90ee7af9949b8e620b0dc24e47b7e
zippermann <88617452+zippermann@users.noreply.github.com>
2024-07-25 16:47:02 +0700

    initial commit

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = "Visit Taman";
+export const SITE_DESCRIPTION = "Explore Madiun's Finest District";
+export const HOMEPAGE_ARTICLE_LIMIT = 4;
+export const ARTICLES_PER_PAGE = 6;

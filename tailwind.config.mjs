1c05ad8c0874ad0c5e39962410cbc34399744e3a
zippermann <88617452+zippermann@users.noreply.github.com>
2024-07-29 12:44:40 +0700

    color + language change

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,16 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				'tt-teal': '#44878c',
+				'tt-lightteal': '#95c1c5',
+				'tt-darkteal': '#2f5e61',
+				'tt-yellow': '#f7dd8a',
+				'tt-green': '#82aa59',
+				'tt-lightgreen': '#becc7b'
+			}
+		},
 	},
 	plugins: [],
 }
